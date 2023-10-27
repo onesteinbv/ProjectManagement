e5a12780867ea72a2788b01b429933e979d8ff39
anjeel.haria <anjeel.haria@brainvire.com>
2023-10-27 17:28:55 +0530

    Updated priority field for helpdesk ticket

--- helpdesk_ticket_custom_priority/models/helpdesk_ticket.py
+++ helpdesk_ticket_custom_priority/models/helpdesk_ticket.py
@@ -6,10 +6,10 @@ from odoo import api, fields, models
 class HelpdeskTicket(models.Model):
     _inherit = "helpdesk.ticket"
 
-    priority = fields.Selection([('0', 'Deferred Priority'),
-                                 ('1', 'Low Priority'),
-                                 ('2', 'Medium Priority'),
-                                 ('3', 'High Priority'),
-                                 ('4', 'Critical Priority'),
-                                 ('5', 'Blocker Priority'),
-                                 ], default="0", string="Priority")
+    priority = fields.Selection(selection_add=[('0', 'Deferred Priority'),
+                                               ('1', 'Low Priority'),
+                                               ('2', 'Medium Priority'),
+                                               ('3', 'High Priority'),
+                                               ('4', 'Critical Priority'),
+                                               ('5', 'Blocker Priority'),
+                                               ], default="0", string="Priority")
