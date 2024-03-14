3d185eeee80ab222e483b06e3de2e3bfdd442450
Anjeel <bytemeasap@gmail.com>
2024-03-14 13:52:38 +0530

    [UPD]Changes to avoid warnings

--- helpdesk_mgmt_email/models/helpdesk_ticket_team.py
+++ helpdesk_mgmt_email/models/helpdesk_ticket_team.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class HelpdeskTeam(models.Model):
     _inherit = "helpdesk.ticket.team"
 
-    email = fields.Char(help="This would be used when sending out emails to contacts")
+    email = fields.Char('Team Email', help="This would be used when sending out emails to contacts")
