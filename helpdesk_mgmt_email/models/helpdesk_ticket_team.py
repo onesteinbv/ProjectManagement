3a4863c7d08089896c0bac6d7c4f2abe5837ec67
Anjeel <bytemeasap@gmail.com>
2024-03-15 11:40:59 +0530

    Update Email Module and add submodules

--- helpdesk_mgmt_email/models/helpdesk_ticket_team.py
+++ helpdesk_mgmt_email/models/helpdesk_ticket_team.py
@@ -4,4 +4,6 @@ from odoo import fields, models
 class HelpdeskTeam(models.Model):
     _inherit = "helpdesk.ticket.team"
 
-    email = fields.Char('Team Email', help="This would be used when sending out emails to contacts")
+    email = fields.Char(
+        "Team Email", help="This would be used when sending out emails to contacts"
+    )
