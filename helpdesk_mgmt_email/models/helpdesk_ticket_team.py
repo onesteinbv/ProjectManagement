dcf75ef7b3fa3e5f1fb70858c5eb78a667de76db
Anjeel Haria <bytemeasap@gmail.com>
2025-02-13 18:14:48 +0530

    Removed helpdesk_mgmt_email,helpdesk_mgmt_fetchmail_multicompany and helpdesk_ticket_custom_priority modules

--- helpdesk_mgmt_email/models/helpdesk_ticket_team.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class HelpdeskTeam(models.Model):
-    _inherit = "helpdesk.ticket.team"
-
-    email = fields.Char(
-        "Team Email", help="This would be used when sending out emails to contacts"
-    )
